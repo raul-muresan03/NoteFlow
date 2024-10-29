ce63ec1af45af6fc6b4def980113600f965e2849
raul <raulmuresancalin@gmail.com>
2024-10-29 10:50:46 +0200

    I worked on the Landing Page

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export const Heading = () => {
+    return ( 
+        <div className="max-w-3xl space-y-4">
+            <h1>
+                Your Ideas, Documents & Plans in a Single Place! Welcome to 
+                <span className="underline">
+                    Noteflow
+                </span>
+            </h1>
+            
+        </div>
+     );
+}
