05c08d3ada3840ad22ec0ebb58b9d43ec09c67d7
raul <raulmuresancalin@gmail.com>
2024-11-02 20:21:14 +0200

    Added Auth and configured Convex

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,10 +1,16 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export const Heading = () => {
-    return ( 
+
+    const { isAuthenticated, isLoading } = useConvexAuth();
+    return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
                 Your Ideas, Documents & Plans in a Single Place! Welcome
@@ -13,13 +19,31 @@ export const Heading = () => {
                 </span>
             </h1>
             <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-                NoteFlow is the connected workspace where <br/>
+                NoteFlow is the connected workspace where <br />
                 better and more organized work happens.
             </h3>
-            <Button>
-                Enter NoteFlow
-                <ArrowRight className="h-4 w-4 ml-2" />
-            </Button>
+            {isLoading && (
+                <div className="w-full flex items-center justify-center">
+                    <Spinner size="lg" />
+                </div>
+            )}
+            {isAuthenticated && !isLoading && (
+                <Button asChild>
+                    <Link href="/documents">
+                        Enter NoteFlow
+                        <ArrowRight className="h-4 w-4 ml-2" />
+                    </Link>
+                </Button>
+            )}
+            {!isAuthenticated && !isLoading && (
+                <SignInButton mode="modal">
+                    <Button>
+                        Get NoteFlow free
+                        <ArrowRight className="h-4 w-4 ml-2"/>
+                    </Button>
+                </SignInButton>
+            )}
+
         </div>
-     );
+    );
 }
---
+++ convex/auth.config.js
@@ -0,0 +1,9 @@
+export default {
+    providers: [
+        {
+            domain:"https://light-ewe-79.clerk.accounts.dev",
+            applicationID: "convex",
+            
+        }
+    ]
+}
