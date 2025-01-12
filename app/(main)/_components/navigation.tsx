30b55df37de74d9ede67e5f897733dd56cfc4cc2
raul <raulmuresancalin@gmail.com>
2025-01-12 09:07:48 +0200

    Add document detail page and navbar component; implement document fetching and title editing functionality

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from "@/lib/utils"
 import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from "lucide-react"
-import { usePathname } from "next/navigation"
+import { useParams, usePathname } from "next/navigation"
 import { ElementRef, useEffect, useRef, useState } from "react"
 import { useMediaQuery } from "usehooks-ts"
 import { UserItem } from "./user-item"
@@ -20,13 +20,15 @@ import {
 import { useSearch } from "@/hooks/use-search"
 import { useSettings } from "@/hooks/use-settings"
 import { TrashBox } from "./trash-box"
+import { Navbar } from "./navbar"
 
 export const Navigation = () => {
-    const search = useSearch();     //foloseste hook-ul useSearch pentru a cauta in documente
-    const settings = useSettings();
     const isMobile = useMediaQuery("(max-width: 768px)");       //foloseste hook-ul useMediaQuery pentru a verifica daca ecranul este mai mic de 768px
     const pathname = usePathname();     //calea curenta
     const create = useMutation(api.documents.create);
+    const search = useSearch();     //foloseste hook-ul useSearch pentru a cauta in documente
+    const settings = useSettings();
+    const params = useParams();      //parametrii din URL
 
     const isResizingRef = useRef(false);    //foloseste hook-ul useRef pentru a crea o referinta la un obiect care contine o valoare booleana 
     const sidebarRef = useRef<ElementRef<"aside">>(null);   //foloseste hook-ul useRef pentru a crea o referinta la un obiect de tip aside
@@ -198,9 +200,16 @@ export const Navigation = () => {
                     isMobile && "left-0 w-full"
                 )}
             >
-                <nav className="bg-transparent px-3 py-2 w-full">
-                    {isCollapsed && <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground" />}
-                </nav>
+                {!!params.documentId ? (    //!! converteste valoarea in boolean
+                    <Navbar
+                        isCollapsed={isCollapsed}
+                        onResetWidth={resetWidth}
+                    />
+                ) : (
+                    <nav className="bg-transparent px-3 py-2 w-full">
+                        {isCollapsed && <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground" />}
+                    </nav>
+                )}
             </div>
         </>
     )
