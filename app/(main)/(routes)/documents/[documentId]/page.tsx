0cdf2bb31a4d3748f7131966b3d679a98d8efb54
Raul <raulmuresancalin@gmail.com>
2025-03-17 02:18:27 +0200

    fix

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -8,54 +8,32 @@ import { Cover } from "@/components/cover";
 import { Editor } from "@/components/editor";
 
 interface DocumentIdPageProps {
-    params: {
-        documentId: string;
-    };
-    searchParams?: Record<string, string | string[] | undefined>;
+  params: { documentId: string };
+  searchParams?: { [key: string]: string | string[] };
 }
 
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const documentId = params.documentId as Id<"documents">;
 
-const DocumentIdPage = ({
-    params
-}: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, { documentId });
+  const update = useMutation(api.documents.update);
 
-    const documentId = params.documentId as unknown as Id<"documents">;
+  const onChange = (content: string) => {
+    update({ id: documentId, content });
+  };
 
-    const document = useQuery(api.documents.getById, {
-        documentId
-    });
+  if (document === undefined) return <div>Loading...</div>;
+  if (document === null) return <div>Not found</div>;
 
-
-    const update = useMutation(api.documents.update);
-    const onChange = (content: string) => {
-        update({
-            id: documentId,
-            content
-        });
-    };
-
-    if (document === undefined) {
-        return <div>Loading...</div>;
-    }
-
-    if (document === null) {
-        return <div>Not found</div>;
-    }
-
-    return (
-        <div className="pb-40">
-            <Cover url={document.coverImage} />
-            <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
-                <Toolbar initialData={document} />
-                <Editor
-                    onChange={onChange}
-                    initialContent={document.content}
-                />
-
-                {/* <Editor /> */}
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="pb-40">
+      <Cover url={document.coverImage} />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+        <Editor onChange={onChange} initialContent={document.content} />
+      </div>
+    </div>
+  );
+};
 
 export default DocumentIdPage;
