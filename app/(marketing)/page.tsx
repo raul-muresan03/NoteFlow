0264ca8c91324d08eaac23bd0bfe35775f22f83c
Raul <raulmuresancalin@gmail.com>
2025-03-17 03:46:11 +0200

    fix: update background styles for marketing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,7 +3,7 @@ import { Heroes } from "./_components/heroes";
 
 const MarketingPage = () => {
   return (
-    <div className="min-h-full flex flex-col">
+    <div className="min-h-screen flex flex-col bg-white dark:bg-[#1F1F1F]">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-18 flex-1 px-6 pb-10">
         <Heading />
         <Heroes />
